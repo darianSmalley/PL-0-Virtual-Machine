07c8a0d8d6496c359925f2886c83ab3e6154b1f5
Darian <darian.smalley@gmail.com>
2015-06-30 21:26:26 -0400

    parser header

---
+++ 0 VM/parser.h
@@ -0,0 +1,25 @@
+//
+//  parser.h
+//  0 VM
+//
+//  Created by Darian Smalley on 6/29/15.
+//  Copyright (c) 2015 Darian Smalley. All rights reserved.
+//
+
+#ifndef ____VM__parser__
+#define ____VM__parser__
+
+#include <stdio.h>
+
+int parse(char* fileName);
+FILE* openFile(char* fileName);
+char* fillInputStream(char* fileName);
+void getToken();
+void PROGRAM();
+void BLOCK();
+void STATEMENT();
+void EXPRESSION();
+void CONDITION();
+void TERM();
+void FACTOR();
+#endif /* defined(____VM__parser__) */
